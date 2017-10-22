0785210703cd2b342ccc81f59efd7096bba18446
Pawel B <wodnik1985@gmail.com>
2017-10-22 15:34:46 +0200

    adding first commit with content

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        String input = "4\n" +
+                "2 3\n" +
+                "0 2\n" +
+                "1 0\n" +
+                "2 2";
+        Scanner sc = new Scanner(input);
+
+        int boardSize = sc.nextInt();
+
+        int[] coordinates = new int[2 * boardSize];
+        for (int i = 0; i < 2 * boardSize; i++) {
+            coordinates[i] = sc.nextInt();
+        }
+
+        int[][] pairedCoord = new int[boardSize][];
+        int n = 1;
+        for (int j = 0; j < boardSize; j++) {
+            int[] tempCoord = new int[2];
+            tempCoord[0] = coordinates[n - 1];
+            tempCoord[1] = coordinates[n];
+            pairedCoord[j] = tempCoord;
+            n = n + 2;
+        }
+
+        String result = "CORRECT";
+
+        for (int x = 0; x < pairedCoord.length; x++) {
+            int x1 = pairedCoord[x][0];
+            int y1 = pairedCoord[x][1];
+            int x2 = pairedCoord[x + 1][0];
+            int y2 = pairedCoord[x + 1][1];
+            if (x1 == x2 || y1 == y2 || (Math.abs(x1 - y1) == Math.abs(x2 - y2))) {
+                result = "INCORRECT";
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
