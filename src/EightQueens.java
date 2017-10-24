66a24530d15a967de2147d72c3549509f200f930
pawel.blaszyk <wodnik1985@gmail.com>
2017-10-24 07:46:26 +0200

    Queens class moved outside the package, EightQueens class added for another task

---
+++ src/EightQueens.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EightQueens {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String input = "1\n" +
+//                "0 0";
+//        Scanner sc = new Scanner(input);
+
+        int boardSize = sc.nextInt();
+    }
+}
